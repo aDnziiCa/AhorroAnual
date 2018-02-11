42547b15a0c363644bab0be18575f22e95b258e4
aDnziiCa <36384643+aDnziiCa@users.noreply.github.com>
2018-02-11 15:39:47 -0600

    Initial commit

---
+++ app/src/main/java/adnziica/com/ahorroanual/AdapterListSavings.java
@@ -0,0 +1,11 @@
+package adnziica.com.ahorroanual;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by aDnziiCa on 11/02/2018.
+ */
+
+public class AdapterListSavings extends RecyclerView.Recycler {
+
+}
